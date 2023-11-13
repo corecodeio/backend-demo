c3d98e8bbcf67c335107af46c39942abaf5feeb8
Jose Leonardo Agreda Anchaise <joseleonardoagreda@gmail.com>
2023-11-13 19:20:05 -0300

    issue 1 solucionado

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const users = [];
 
 app.get("/users", function (request, response) {
-  response.status(200).json({ massage: "Get all users", users });
+  response.status(200).json({ massage: "toda la lista de usuarios", users });
 });
 
 app.listen(config.port, () => {
